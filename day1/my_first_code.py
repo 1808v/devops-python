2e037877d985e33fe93aff8cddf90d4834027125
vivek-nickelfox <vivek@nickelfox.com>
2024-06-24 22:27:31 +0530

    day01

---
+++ day1/my_first_code.py
@@ -0,0 +1,5 @@
+env=input("Enter the Cloud Plateform:")
+if env == "aws" or env == "AWS":
+    print("you are using AWS cloud service ")
+else:
+    print("you are not in aws but in another cloud plateform")
