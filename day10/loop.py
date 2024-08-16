199372b7cd035592af853bcb7e2062cb374a9e3f
vivek <vivek@Vipuls-MacBook-Pro.local>
2024-08-16 16:01:10 +0530

    adding .gitignore

---
+++ day10/loop.py
@@ -0,0 +1,18 @@
+
+#def table(y):    
+#    for i in range(1,11):
+#        x=i*y
+#        print(x)
+#table(5)
+#
+def table(numb):
+    numb = int(input(" Enter a number : "))    
+
+    # using the for loop to generate the multiplication tables     
+
+    print("Table of: ",numb)  
+
+    for a in range(1,11):    
+
+       print(numb,'x',a,'=',numb*a)  
+table(numb=)
